042bb84f31ebe4ed5daa2ff1e374639dba767bfc
Jan Ackermann <eric-ackermann@hotmail.de>
2023-05-17 23:00:31 +0200

    fix: remove fullscreen from storybook story

--- src/components/TitleHeader/TitleHeader.stories.ts
+++ src/components/TitleHeader/TitleHeader.stories.ts
@@ -11,10 +11,6 @@ const meta = {
     },
     template: '<title-header  />'
   }),
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen'
-  },
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
   tags: ['autodocs']
 } satisfies Meta<typeof TitleHeader>
