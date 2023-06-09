a53be71fd9ce21dc55ef4ced01d01d04ecc9bc8d
Jan Ackermann <eric-ackermann@hotmail.de>
2023-06-09 22:08:03 +0200

    feat: first draft of new post entry component

---
+++ src/components/PostEntry/PostEntry.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="post-entry">
+    <div class="post-entry__img">
+      <img :src="imgSrc" :alt="imgAlt" class="post-entry__img--visible" />
+      <img :src="imgSrc" :alt="imgAlt" class="post-entry__img--bg" />
+    </div>
+    <div class="post-entry__body-wrapper">
+      <h2 class="post-entry__title">
+        <slot name="title" />
+      </h2>
+      <div class="post-entry__text">
+        <slot />
+      </div>
+      <div class="post-entry__actions">
+        <slot name="actions" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  imgSrc: string;
+  imgAlt: string;
+}>();
+</script>
+
+<style lang="scss">
+@import 'PostEntry';
+</style>
