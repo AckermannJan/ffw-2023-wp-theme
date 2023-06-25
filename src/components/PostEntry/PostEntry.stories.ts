9d55247cb27103c5f4bccf1b4a9675495d169567
Jan Ackermann <eric-ackermann@hotmail.de>
2023-06-25 18:56:35 +0200

    Feature add new post entry component (#1)
    * feat: first draft of new post entry component
    
    * feat: Add triangle to post entry
    
    * feat: Add small variant
    
    * fix: Use correct props in doc page

---
+++ src/components/PostEntry/PostEntry.stories.ts
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import PostEntry from './PostEntry.vue';
+import DynamicButton from '../DynamicButton/DynamicButton.vue';
+
+const meta = {
+  component: PostEntry,
+  render: (args: any) => ({
+    components: { PostEntry, DynamicButton },
+    setup() {
+      return { args };
+    },
+    template: `
+      <div style="width: 714px;">
+        <post-entry 
+          :img-src="args.imgSrc" 
+          :img-alt="args.imgAlt" 
+          :variant="args.variant"
+        >
+          <template #title>
+            Maiwanderung
+          </template>
+          Die Wanderung am 1. Mai eines jeden Jahres ist ein Brauch, den wir bei der 
+          Feuerwehr Traisa bereits seit Jahrzehnten pflegen. So ging es auch in diesem Jahr
+          wieder auf Umwegen zum Traaser Hüttchen, wo uns das Waldfest der Chorgemeinde Traisa erwartet
+          wieder auf Umwegen zum Traaser Hüttchen, wo uns das Waldfest der Chorgemeinde Traisa erwartet
+          wieder auf Umwegen zum Traaser Hüttchen, wo uns das Waldfest der Chorgemeinde Traisa erwartet
+          <template #actions>
+            <DynamicButton>
+              WEITERLESEN
+            </DynamicButton>
+          </template>
+        </post-entry>
+      </div>
+    `,
+  }),
+  tags: ['autodocs'],
+} satisfies Meta<typeof PostEntry>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    imgSrc: 'http://wordpress.feuerwehr-traisa.de/wp-content/uploads/2023/05/IMG_5254.jpeg',
+    imgAlt: 'Alt text',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    imgSrc: 'http://wordpress.feuerwehr-traisa.de/wp-content/uploads/2023/05/IMG_5254.jpeg',
+    imgAlt: 'Alt text',
+    variant: 'small',
+  },
+};
---
+++ src/components/PostEntry/PostEntry.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="['post-entry', { 'post-entry--small': variant === 'small' }]">
+    <div :class="['post-entry__img', { 'post-entry__img--small': variant === 'small' }]">
+      <img :src="imgSrc" :alt="imgAlt" class="post-entry__img--visible" />
+      <img :src="imgSrc" :alt="imgAlt" class="post-entry__img--bg" />
+    </div>
+    <div
+      :class="[
+        'post-entry__body-wrapper',
+        { 'post-entry__body-wrapper--small': variant === 'small' },
+      ]"
+    >
+      <div>
+        <h2 :class="['post-entry__title', { 'post-entry__title--small': variant === 'small' }]">
+          <slot name="title" />
+        </h2>
+        <div :class="['post-entry__text', { 'post-entry__text--small': variant === 'small' }]">
+          <slot />
+        </div>
+      </div>
+      <div class="post-entry__actions">
+        <slot name="actions" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  imgSrc: string;
+  imgAlt: string;
+  variant: string;
+}>();
+</script>
+
+<style lang="scss">
+@import 'PostEntry';
+</style>
